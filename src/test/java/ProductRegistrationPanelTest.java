bb9cbf0854e04d2aaea607dc9ec2d11247e9342b
skemex <skemex@gmail.com>
2023-05-11 00:30:45 -0600

    Update ProductRegistrationPanelTest.java

--- src/test/java/ProductRegistrationPanelTest.java
+++ src/test/java/ProductRegistrationPanelTest.java
@@ -6,7 +6,7 @@ import javax.swing.JTextField;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductRegistrationPanelTest {
