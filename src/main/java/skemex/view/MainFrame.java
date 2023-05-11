fd7ab534f75fd1294c2db0ef33246d28708010c6
skemex <skemex@gmail.com>
2023-05-10 23:39:52 -0600

    add MainFrame.java

---
+++ src/main/java/skemex/view/MainFrame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+            setTitle("Product Registration");
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            
+                                    // Create and add ProductRegistrationPanel to frame
+                                            ProductRegistrationPanel panel = new ProductRegistrationPanel();
+                                                    add(panel);
+                                                            
+                                                                    pack(); // Size the frame to fit its contents
+                                                                            setLocationRelativeTo(null); // Center the frame on the screen
+                                                                                }
+                                                                                    
+                                                                                        public static void main(String[] args) {
+                                                                                                // Run the frame on the event dispatch thread
+                                                                                                        SwingUtilities.invokeLater(() -> {
+                                                                                                                    ProductRegistrationFrame frame = new ProductRegistrationFrame();
+                                                                                                                                frame.setVisible(true);
+                                                                                                                                        });
+                                                                                                                                            }
+                                                                                                                                            }
